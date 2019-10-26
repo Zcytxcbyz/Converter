e3941be03d684bace0a47a76c39b4c3462b03384
zcytxcbyz@163.com <zcytxcbyz@163.com>
2019-10-26 20:10:31 +0800

    Update

---
+++ Converter/MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Converter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_num = new System.Windows.Forms.ComboBox();
+            this.comboBox_result = new System.Windows.Forms.ComboBox();
+            this.label_num = new System.Windows.Forms.Label();
+            this.label_result = new System.Windows.Forms.Label();
+            this.textBox_num = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.button_Main = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox_num
+            // 
+            this.comboBox_num.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_num.FormattingEnabled = true;
+            this.comboBox_num.Items.AddRange(new object[] {
+            "2进制",
+            "8进制",
+            "10进制",
+            "16进制"});
+            this.comboBox_num.Location = new System.Drawing.Point(177, 6);
+            this.comboBox_num.Name = "comboBox_num";
+            this.comboBox_num.Size = new System.Drawing.Size(71, 20);
+            this.comboBox_num.TabIndex = 0;
+            // 
+            // comboBox_result
+            // 
+            this.comboBox_result.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_result.FormattingEnabled = true;
+            this.comboBox_result.Items.AddRange(new object[] {
+            "2进制",
+            "8进制",
+            "10进制",
+            "16进制"});
+            this.comboBox_result.Location = new System.Drawing.Point(177, 33);
+            this.comboBox_result.Name = "comboBox_result";
+            this.comboBox_result.Size = new System.Drawing.Size(71, 20);
+            this.comboBox_result.TabIndex = 1;
+            // 
+            // label_num
+            // 
+            this.label_num.AutoSize = true;
+            this.label_num.Location = new System.Drawing.Point(12, 9);
+            this.label_num.Name = "label_num";
+            this.label_num.Size = new System.Drawing.Size(53, 12);
+            this.label_num.TabIndex = 2;
+            this.label_num.Text = "转换数字";
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.Location = new System.Drawing.Point(12, 36);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(53, 12);
+            this.label_result.TabIndex = 3;
+            this.label_result.Text = "转换结果";
+            // 
+            // textBox_num
+            // 
+            this.textBox_num.Location = new System.Drawing.Point(71, 6);
+            this.textBox_num.Name = "textBox_num";
+            this.textBox_num.Size = new System.Drawing.Size(100, 21);
+            this.textBox_num.TabIndex = 4;
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(71, 33);
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(100, 21);
+            this.textBox_result.TabIndex = 5;
+            // 
+            // button_Main
+            // 
+            this.button_Main.Location = new System.Drawing.Point(96, 60);
+            this.button_Main.Name = "button_Main";
+            this.button_Main.Size = new System.Drawing.Size(75, 23);
+            this.button_Main.TabIndex = 6;
+            this.button_Main.Text = "转换";
+            this.button_Main.UseVisualStyleBackColor = true;
+            this.button_Main.Click += new System.EventHandler(this.button_Main_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 94);
+            this.Controls.Add(this.button_Main);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.textBox_num);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.label_num);
+            this.Controls.Add(this.comboBox_result);
+            this.Controls.Add(this.comboBox_num);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "进制转换";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_num;
+        private System.Windows.Forms.ComboBox comboBox_result;
+        private System.Windows.Forms.Label label_num;
+        private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.TextBox textBox_num;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Button button_Main;
+    }
+}
+
---
+++ Converter/MainForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Converter
+{
+    public partial class MainForm : Form
+    {
+        public int[] Index = { 2, 8, 10, 16 };
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            comboBox_num.SelectedIndex = 0;
+            comboBox_result.SelectedIndex = 2;
+        }
+
+        private void button_Main_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int valueindex = Index[comboBox_num.SelectedIndex];
+                int resultindex = Index[comboBox_result.SelectedIndex];
+                textBox_result.Text = NumConverter(textBox_num.Text, valueindex, resultindex);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text);
+            }
+        }
+        private string NumConverter(string value,int valueindex,int resultindex)
+        {
+            if (NumAnalysis(value, valueindex))
+            {
+                string result = null; int temp = 0;
+                if (valueindex != 10)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        int strindex = value.Length - i - 1;
+                        string item = value[strindex].ToString();
+                        if (Regex.IsMatch(item.ToString(), "[A-Z]"))
+                            item = (value[strindex] - 'A' + 10).ToString();
+                        else if (Regex.IsMatch(item.ToString(), "[a-z]"))
+                            item = (value[strindex] - 'a' + 10).ToString();
+                        temp += Convert.ToInt32(item) * Convert.ToInt32(Math.Pow(valueindex, i));
+                    }
+                }
+                else
+                {
+                    temp = Convert.ToInt32(value);
+                }
+
+                while (temp > 0)
+                {
+                    int item = temp % resultindex;
+                    if (item < 10)
+                        result = item.ToString() + result;
+                    else
+                        result += ((char)(item - 10 + 'a')).ToString() + result;
+
+                    temp /= resultindex;
+                }
+                return result;
+            }
+            else
+            {
+                throw new FormatException("输入字符串的格式不正确。");
+            }
+        }
+        private bool NumAnalysis(string value,int valueindex)
+        {
+            bool result = false;
+            if (valueindex == 10)
+                result = Regex.IsMatch(value, "^[0-9]+$");
+            if (valueindex == 2)
+                result = Regex.IsMatch(value, "^[01]+$");
+            if (valueindex == 8)
+                result = Regex.IsMatch(value, "^[0-7]+$");
+            if (valueindex == 16)
+                result = Regex.IsMatch(value, "^[0-9A-Fa-f]+$");
+            return result;
+        }
+    }
+}
